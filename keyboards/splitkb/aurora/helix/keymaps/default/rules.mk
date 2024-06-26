d099cf612102c41982ec7bfb9aeb62c80641074d
r4b2 <github@r4b2.de>
2024-06-26 09:58:23 +0200

    wip

--- keyboards/splitkb/aurora/helix/keymaps/default/rules.mk
+++ keyboards/splitkb/aurora/helix/keymaps/default/rules.mk
@@ -4,7 +4,7 @@
 
 # RGB Matrix is enabled at the revision level,
 # while we use the regular RGB underglow for testing
-RGB_MATRIX_ENABLE = no
+RGB_MATRIX_ENABLE = yes
 RGBLIGHT_ENABLE = yes
 
 CONVERT_TO=liatris
@@ -13,3 +13,5 @@ QMK_SETTINGS = yes
 LTO_ENABLE = yes
 
 RGBLIGHT_SLEEP = yes
+
+VIA_ENABLE = yes
