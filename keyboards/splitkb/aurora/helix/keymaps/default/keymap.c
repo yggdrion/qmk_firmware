9a7e498f82f3f22c1e5fb6eead708426eb7af081
r4b2 <github@r4b2.de>
2024-06-29 02:32:31 +0200

    wip

--- keyboards/splitkb/aurora/helix/keymaps/default/keymap.c
+++ keyboards/splitkb/aurora/helix/keymaps/default/keymap.c
@@ -6,26 +6,34 @@
  * edit it directly.
  */
 
+void keyboard_pre_init_user(void) {
+    // Set our LED pin as output
+    setPinOutput(24);
+    // Turn the LED off
+    // (Due to technical reasons, high is off and low is on)
+    writePinHigh(24);
+}
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[0] = LAYOUT(   KC_GRV,       KC_1,        KC_2,    KC_3,  KC_4,   KC_5,                    KC_6,    KC_7,   KC_8, KC_9, KC_0, KC_BSPC,
-                KC_TAB,       KC_Q,        KC_W,    KC_E,  KC_R,   KC_T,                    KC_Y,    KC_U,   KC_I, KC_O, KC_P, KC_BSLS,
-                KC_BSPC,      KC_A,        KC_S,    KC_D,  KC_F,   KC_G,                    KC_H,    KC_J,   KC_K,    KC_L, KC_SCLN, KC_QUOT,
-                KC_LSFT,      KC_Z,        KC_X,    KC_C,  KC_V,   KC_B,    KC_NO,  KC_NO,  KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_EQL,
-                KC_LEFT_CTRL, KC_LEFT_GUI, KC_LALT, MO(1), KC_SPC, KC_LBRC, KC_ESC, KC_DEL, KC_RBRC, KC_ENT, MO(2),   KC_NO,  KC_NO,   KC_MINS
+[0] = LAYOUT(   KC_GRV,        KC_1,         KC_2,    KC_3,    KC_4,     KC_5,                      KC_6,    KC_7,   KC_8,    KC_9,   KC_0, KC_BSPC,
+                KC_TAB,        KC_Q,         KC_W,    KC_E,    KC_R,     KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,   KC_P, KC_BSLS,
+                KC_BSPC,       KC_A,         KC_S,    KC_D,    KC_F,     KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+                KC_LSFT,       KC_Z,         KC_X,    KC_C,    KC_V,     KC_B,    KC_L,   KC_R,     KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_EQL,
+                KC_LEFT_CTRL,  KC_LEFT_GUI,  KC_LALT, MO(1),   KC_SPC,   KC_LBRC, KC_ESC, KC_DEL,   KC_RBRC, KC_ENT, MO(2),   KC_NO,  KC_NO,   KC_MINS
 ),
-[1] = LAYOUT(   _______,      _______,     _______, _______, _______, _______,                _______, _______, _______, _______, _______, _______,
-                _______,      KC_TILD,     KC_EXLM, KC_AT, KC_HASH, KC_DLR,                   KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-                _______,      KC_F1,       KC_F2,   KC_F3, KC_F4, KC_F5,                      KC_F6, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
-                _______,      KC_F7,       KC_F8,   KC_F9, KC_F10, KC_F11,   _______, _______, KC_F12, _______, KC_HOME, KC_END, _______, _______,
-                _______,      _______,     _______, MO(3), _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU
+[1] = LAYOUT(   KC_TRNS,       KC_F1,       KC_F2,    KC_F3,   KC_F4,    KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
+                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
-[2] = LAYOUT(   _______,      _______,     _______, _______, _______, _______,                _______, _______, _______, _______, _______, KC_BSPC,
-                _______,      KC_GRV,      KC_1,    KC_2, KC_3, KC_4,                         KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
-                _______,      KC_F1,       KC_F2,   KC_F3, KC_F4, KC_F5,                      KC_F6, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
-                _______,      KC_F7,       KC_F8,   KC_F9, KC_F10, KC_F11,   _______, _______, KC_F12, _______, KC_PGDN, KC_PGUP, _______, _______,
-                _______,      _______,     _______, MO(3), _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU
+[2] = LAYOUT(   KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 )
 
 };
@@ -39,9 +47,9 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
 #ifdef RGBLIGHT_ENABLE
 void keyboard_post_init_user(void) {
-    rgblight_enable_noeeprom();                          // enables RGB, without saving settings
-    rgblight_sethsv_noeeprom(HSV_RED);                   // sets the color to red without saving
-    rgblight_mode_noeeprom(RGBLIGHT_MODE_BREATHING + 3); // sets mode to Fast breathing without saving
+    rgblight_enable_noeeprom();                         // enables RGB, without saving settings
+    rgblight_sethsv_noeeprom(HSV_WHITE);                // sets the color to red without saving
+    rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT); // sets mode to Fast breathing without saving
 }
 #endif
 
@@ -50,17 +58,21 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     // 0 is left-half encoder
     // 1 is right-half encoder
     if (index == 0) {
-        tap_code(KC_L);
+        if (clockwise) {
+            // increase volume
+            tap_code(KC_MS_WH_DOWN);
+        } else {
+            // decrease volume
+            tap_code(KC_MS_WH_UP);
+        }
     } else if (index == 1) {
-        tap_code(KC_R);
-    }
-
-    if (clockwise) {
-        // increase volume
-        tap_code(KC_VOLU);
-    } else {
-        // decrease volume
-        tap_code(KC_VOLD);
+        if (clockwise) {
+            // increase volume
+            tap_code(KC_VOLU);
+        } else {
+            // decrease volume
+            tap_code(KC_VOLD);
+        }
     }
 
     return false;
