511ee05186bf2feb45d1053f8059ccfbbeec4c36
r4b2 <github@r4b2.de>
2024-06-30 09:58:55 +0200

    wip

--- keyboards/splitkb/aurora/helix/keymaps/default/keymap.c
+++ keyboards/splitkb/aurora/helix/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_TAB,        KC_Q,         KC_W,    KC_E,    KC_R,     KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_BSLS,
                 KC_BSPC,       KC_A,         KC_S,    KC_D,    KC_F,     KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT,
                 KC_LSFT,       KC_Z,         KC_X,    KC_C,    KC_V,     KC_B,    KC_L,   KC_R,     KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_EQL,
-                KC_LEFT_CTRL,  KC_LEFT_GUI,  KC_LALT, KC_LBRC, KC_SPC,   MO(0),   KC_ESC, KC_DEL,   MO(0),   KC_ENT, KC_RBRC, KC_NO,  KC_NO,   KC_MINS
+                KC_LEFT_CTRL,  KC_LEFT_GUI,  KC_LALT, KC_LBRC, KC_SPC,   MO(1),   KC_ESC, KC_DEL,   MO(2),   KC_ENT, KC_RBRC, KC_NO,  KC_NO,   KC_MINS
 ),
 [1] = LAYOUT(   KC_TRNS,       KC_F1,       KC_F2,    KC_F3,   KC_F4,    KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
                 KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -66,23 +66,20 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     // 1 is right-half encoder
     if (index == 0) {
         if (clockwise) {
-            // increase volume
             tap_code(KC_VOLU);
         } else {
-            // decrease volume
             tap_code(KC_VOLD);
         }
 
     } else if (index == 1) {
         if (clockwise) {
-            // increase volume
-            tap_code(KC_MS_WH_DOWN);
-        } else {
-            // decrease volume
-            tap_code(KC_MS_WH_UP);
+            if (clockwise) {
+                tap_code(KC_PGDN);
+            } else {
+                tap_code(KC_PGUP);
+            }
         }
-    }
 
-    return false;
-}
+        return false;
+    }
 #endif
