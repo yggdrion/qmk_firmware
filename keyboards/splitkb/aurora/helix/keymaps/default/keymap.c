bd69872e1019cf8bde794f49deaa79dc3004a866
r4b2 <github@r4b2.de>
2024-07-03 01:27:51 +0200

    fix

--- keyboards/splitkb/aurora/helix/keymaps/default/keymap.c
+++ keyboards/splitkb/aurora/helix/keymaps/default/keymap.c
@@ -26,14 +26,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [1] = LAYOUT(   KC_F11,        KC_F1,       KC_F2,    KC_F3,   KC_F4,    KC_F5,                         KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F12,
                 KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_MINS,  KC_LBRC,                       KC_RBRC, KC_EQL,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                 KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_LCBR,                       KC_RCBR, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_LPRN,   KC_TRNS, KC_TRNS,   KC_RPRN, KC_TRNS,  KC_TRNS, KC_TRNS, KC_DOWN, KC_TRNS,
+                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_LPRN,   KC_TRNS, KC_TRNS,   KC_RPRN, KC_TRNS,  KC_TRNS, KC_TRNS, KC_UP, KC_TRNS,
                 KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 [2] = LAYOUT(   KC_TRNS,       KC_P1,       KC_P2,    KC_P3,   KC_P4,    KC_P5,                         KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_TRNS,
                 KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_MINS,  KC_LBRC,                       KC_RBRC, KC_EQL, KC_TRNS, KC_TRNS, KC_MPLY, KC_TRNS,
                 KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_LCBR,                       KC_RCBR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_LPRN,   KC_TRNS, KC_TRNS,   KC_RPRN, KC_F24,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS,   KC_F21,  KC_F23,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_LPRN,   KC_TRNS, KC_TRNS,   KC_RPRN, KC_F24,  KC_TRNS, KC_TRNS, KC_UP, KC_TRNS,
+                KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS,   KC_F21,  KC_F23,  KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT
 )
 
 };
