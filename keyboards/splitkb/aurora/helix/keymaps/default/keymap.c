9998fb6b336afdf820a354f3cc46498d6d3d476a
r4b2 <github@r4b2.de>
2024-07-03 00:25:15 +0200

    wip

--- keyboards/splitkb/aurora/helix/keymaps/default/keymap.c
+++ keyboards/splitkb/aurora/helix/keymaps/default/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 KC_TAB,        KC_Q,         KC_W,    KC_E,    KC_R,     KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     KC_BSLS,
                 KC_BSPC,       KC_A,         KC_S,    KC_D,    KC_F,     KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
                 KC_LSFT,       KC_Z,         KC_X,    KC_C,    KC_V,     KC_B,    KC_L,   KC_R,     KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_EQL,
-                KC_LEFT_CTRL,  KC_LEFT_GUI,  KC_LALT, KC_LBRC, KC_SPC,   MO(1),   KC_ESC, KC_DEL,   MO(2),   KC_ENT,  KC_RBRC, KC_NO,  KC_NO,    KC_MINS
+                KC_LEFT_CTRL,  KC_LEFT_GUI,  KC_LALT, KC_LBRC, KC_SPC,   MO(1),   KC_ESC, KC_DEL,   MO(2),   KC_ENT,  KC_RBRC, KC_HOME,KC_END,    KC_MINS
 ),
 [1] = LAYOUT(   KC_F11,        KC_F1,       KC_F2,    KC_F3,   KC_F4,    KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
                 KC_TRNS,       KC_TRNS,     KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_TRNS,
@@ -44,12 +44,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 void keyboard_post_init_user(void) {
     // Initialize RGB to static black
     rgblight_enable_noeeprom();
-    rgblight_sethsv_noeeprom(HSV_WHITE);
+    rgblight_sethsv_noeeprom(HSV_BLUE);
     rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT);
 }
 
 void housekeeping_task_user(void) {
-    rgblight_setrgb_at(RGB_WHITE, 0);
+    rgblight_setrgb_at(RGB_BLUE, 0);
 }
 
 #endif
@@ -73,11 +73,9 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 
     } else if (index == 1) {
         if (clockwise) {
-            if (clockwise) {
-                tap_code(KC_PGDN);
-            } else {
-                tap_code(KC_PGUP);
-            }
+            tap_code(KC_PGDN);
+        } else {
+            tap_code(KC_PGUP);
         }
     }
     return false;
