929c4235714a4c201946685ed64a179229d80645
r4b2 <github@r4b2.de>
2024-07-05 15:04:42 +0200

    wip

--- keyboards/splitkb/aurora/helix/keymaps/default/rules.mk
+++ keyboards/splitkb/aurora/helix/keymaps/default/rules.mk
@@ -1,6 +1,7 @@
 # NOTE: These are already enabled by default at the revision level
 #ENCODER_ENABLE = yes
 #OLED_ENABLE = yes
+# WS2812_DRIVER = vendor # Use the RP2040's PIO interface
 
 # RGB Matrix is enabled at the revision level,
 # while we use the regular RGB underglow for testing
@@ -9,11 +10,10 @@ RGBLIGHT_ENABLE = yes
 
 CONVERT_TO=liatris
 
-QMK_SETTINGS = yes
-LTO_ENABLE = yes
+#QMK_SETTINGS = yes
+#LTO_ENABLE = yes
 
 #RGBLIGHT_SLEEP = yes
 
-VIA_ENABLE = yes
+#VIA_ENABLE = yes
 
-# WS2812_DRIVER = vendor # Use the RP2040's PIO interface
